6667bfb38b53442f09ff86b2501ce4543afb7c65
ignaciogregorio <nachitogregorio@hotmail.com>
2022-03-08 14:23:49 -0300

    Creando Repositorio con react

---
+++ src/navbar/Navbar.js
@@ -0,0 +1,26 @@
+
+
+import React from 'react'
+
+import './navbar.css'
+
+export const Navbar = () => {
+
+
+  return (
+
+    <div className='navbar-container'>
+        <img src="" alt="" />IMG
+
+        <ul className='navbar-menu'>
+          <li> Nosotros </li>
+          <li> Servicios </li>
+          <li> Proyectos </li>
+          <li> Contacto </li>
+        </ul>
+
+        <hr />
+
+    </div>
+  )
+}
